8a64475efcb9cbff5afa27ccc9e6452894048ff6
trongkhoa2201 <84830390+trongkhoa2201@users.noreply.github.com>
2023-02-11 23:50:49 +0700

    Create ReactJS Project

---
+++ my-app/src/routers/Routers.js
@@ -0,0 +1,24 @@
+import { Routes , Route, Navigate} from 'react-router-dom'
+
+import Home from '../pages/Home'
+import Cart from '../pages/Cart'
+import Checkout from '../pages/Checkout'
+import Login from '../pages/Login'
+import ProductDetails from '../pages/ProductDetails'
+import Shop from '../pages/Shop'
+import Signup from '../pages/Signup'
+
+const Routers = () => {
+  return <Routes>
+    <Route path='/' element={<Navigate to={'home'}/>} />
+    <Route path='home' element={<Home/>} />
+    <Route path='cart' element={<Cart/>} />
+    <Route path='checkout' element={<Checkout/>} />
+    <Route path='login' element={<Login/>} />
+    <Route path='shop/:id' element={<ProductDetails/>} />
+    <Route path='shop' element={<Shop/>}/>
+    <Route path='signup' element={<Signup/>} />
+  </Routes>
+};
+
+export default Routers
