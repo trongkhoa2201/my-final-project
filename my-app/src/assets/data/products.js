f646d9fc520558be5cba15f92dc9c7051bfd20b6
trongkhoa2201 <84830390+trongkhoa2201@users.noreply.github.com>
2023-02-16 13:01:22 +0700

    Build Product

---
+++ my-app/src/assets/data/products.js
@@ -0,0 +1,191 @@
+import productImg01 from "../images/product-01.jpg"
+import productImg02 from "../images/product-02.jpg"
+import productImg03 from "../images/product-03.jpg"
+import productImg04 from "../images/product-04.jpg"
+import productImg06 from "../images/product-06.jpg"
+import productImg07 from "../images/product-07.jpg"
+import productImg08 from "../images/product-08.jpg"
+import productImg09 from "../images/product-09.jpg"
+
+const products = [
+    {
+      id: "01",
+      productName: "SQUALANE + 10% LACTIC ACID RESURFACING SERUM",
+      imgUrl: productImg01,
+      category: "Biossance",
+      price: 65,
+      shortDesc:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur iure quas illo voluptates labore tempore!",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio nostrum accusantium iste, voluptas cumque provident! Consequatur officiis animi rem tempore voluptate cumque hic similique aperiam ut consectetur distinctio repudiandae quia quam quos, quas illo, iusto, necessitatibus odio veniam exercitationem quis voluptatibus debitis laboriosam! Esse debitis obcaecati blanditiis at impedit quibusdam!",
+      reviews: [
+        {
+          rating: 4.7,
+          text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        },
+      ],
+      avgRating: 4.5,
+    },
+    {
+      id: "02",
+      productName: "SQUALANE + ANTIOXIDANT CLEANSING OIL",
+      imgUrl: productImg02,
+      category: "Biossance",
+      price: 65,
+      shortDesc:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur iure quas illo voluptates labore tempore!",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio nostrum accusantium iste, voluptas cumque provident! Consequatur officiis animi rem tempore voluptate cumque hic similique aperiam ut consectetur distinctio repudiandae quia quam quos, quas illo, iusto, necessitatibus odio veniam exercitationem quis voluptatibus debitis laboriosam! Esse debitis obcaecati blanditiis at impedit quibusdam!",
+      reviews: [
+        {
+          rating: 4.8,
+          text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        },
+        {
+          rating: 4.8,
+          text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        },
+      ],
+      avgRating: 4.7,
+    },
+  
+    {
+      id: "03",
+      productName: "SQUALANE + VITAMIN C DARK SPOT SERUM",
+      imgUrl: productImg03,
+      category: "Biossance",
+      price: 51.80,
+      shortDesc:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur iure quas illo voluptates labore tempore!",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio nostrum accusantium iste, voluptas cumque provident! Consequatur officiis animi rem tempore voluptate cumque hic similique aperiam ut consectetur distinctio repudiandae quia quam quos, quas illo, iusto, necessitatibus odio veniam exercitationem quis voluptatibus debitis laboriosam! Esse debitis obcaecati blanditiis at impedit quibusdam!",
+      reviews: [
+        {
+          rating: 4.6,
+          text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        },
+        {
+          rating: 4.9,
+          text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        },
+      ],
+      avgRating: 4.7,
+    },
+    {
+      id: "04",
+      productName: "SQUALANE + PHYTO-RETINOL SERUM",
+      imgUrl: productImg04,
+      category: "Biossance",
+      price: 32,
+      shortDesc:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur iure quas illo voluptates labore tempore!",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio nostrum accusantium iste, voluptas cumque provident! Consequatur officiis animi rem tempore voluptate cumque hic similique aperiam ut consectetur distinctio repudiandae quia quam quos, quas illo, iusto, necessitatibus odio veniam exercitationem quis voluptatibus debitis laboriosam! Esse debitis obcaecati blanditiis at impedit quibusdam!",
+      reviews: [
+        {
+          rating: 4.6,
+          text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        },
+        {
+          rating: 4.9,
+          text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        },
+      ],
+      avgRating: 4.7,
+    },
+    {
+      id: "06",
+      productName: "The Ordinary Salicylic Acid 2% Solution",
+      imgUrl: productImg06,
+      category: "Best Sales",
+      price: 99,
+      shortDesc:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur iure quas illo voluptates labore tempore!",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio nostrum accusantium iste, voluptas cumque provident! Consequatur officiis animi rem tempore voluptate cumque hic similique aperiam ut consectetur distinctio repudiandae quia quam quos, quas illo, iusto, necessitatibus odio veniam exercitationem quis voluptatibus debitis laboriosam! Esse debitis obcaecati blanditiis at impedit quibusdam!",
+      reviews: [
+        {
+          rating: 4.6,
+          text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        },
+        {
+          rating: 4.9,
+          text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        },
+      ],
+      avgRating: 4.7,
+    },
+  
+    {
+      id: "07",
+      productName: "The Ordinary 100% L-Ascorbic Acid Powde",
+      imgUrl: productImg07,
+      category: "Best Sales",
+      price: 173,
+      shortDesc:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur iure quas illo voluptates labore tempore!",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio nostrum accusantium iste, voluptas cumque provident! Consequatur officiis animi rem tempore voluptate cumque hic similique aperiam ut consectetur distinctio repudiandae quia quam quos, quas illo, iusto, necessitatibus odio veniam exercitationem quis voluptatibus debitis laboriosam! Esse debitis obcaecati blanditiis at impedit quibusdam!",
+      reviews: [
+        {
+          rating: 4.6,
+          text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        },
+        {
+          rating: 4.9,
+          text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        },
+      ],
+      avgRating: 4.7,
+    },
+  
+    {
+      id: "08",
+      productName: "The Ordinary AHA 30% BHA 2% Peeling Solution",
+      imgUrl: productImg08,
+      category: "Best Sales",
+      price: 89,
+      shortDesc:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur iure quas illo voluptates labore tempore!",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio nostrum accusantium iste, voluptas cumque provident! Consequatur officiis animi rem tempore voluptate cumque hic similique aperiam ut consectetur distinctio repudiandae quia quam quos, quas illo, iusto, necessitatibus odio veniam exercitationem quis voluptatibus debitis laboriosam! Esse debitis obcaecati blanditiis at impedit quibusdam!",
+      reviews: [
+        {
+          rating: 4.6,
+          text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        },
+        {
+          rating: 4.9,
+          text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        },
+      ],
+      avgRating: 4.7,
+    },
+  
+    {
+      id: "09",
+      productName: "The Ordinary Mineral UV Filters SPF 30",
+      imgUrl: productImg09,
+      category: "Best Sales",
+      price: 112,
+      shortDesc:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur iure quas illo voluptates labore tempore!",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio nostrum accusantium iste, voluptas cumque provident! Consequatur officiis animi rem tempore voluptate cumque hic similique aperiam ut consectetur distinctio repudiandae quia quam quos, quas illo, iusto, necessitatibus odio veniam exercitationem quis voluptatibus debitis laboriosam! Esse debitis obcaecati blanditiis at impedit quibusdam!",
+      reviews: [
+        {
+          rating: 4.6,
+          text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        },
+        {
+          rating: 4.9,
+          text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        },
+      ],
+      avgRating: 4.7,
+    },
+  
+    
+] 
+
+export default products;
--- my-app/src/pages/Home.js
+++ my-app/src/pages/Home.js
@@ -1,15 +1,30 @@
-import React from 'react'
+import React, {useState ,useEffect} from 'react'
 import '../styles/home.css'
 import Services from '../services/Services'
+import ProductList from '../components/UI/ProductList'
+import products from '../assets/data/products'
 
 import Helmet from '../components/Helmet/Helmet'
 import brand from '../assets/images/brand.jpg'
 import {Container, Row,Col} from "reactstrap"
 import { Link } from 'react-router-dom'
 import { motion } from 'framer-motion'
+import counterImg from '../assets/images/product-05.jpg'
+
 const Home = () => {
   
+  const[recommendProducts, setRecommendProducts] = useState()
+  const[bestSalesProducts, setBestSalesProducts] = useState()
   const year = new Date().getFullYear()
+
+  useEffect(() => {
+    const filterRecommendProducts = products.filter(item => item.category === 'Biossance')
+    const filterBestSalesProducts = products.filter(item => item.category === 'Best Sales')
+
+
+    setRecommendProducts(filterRecommendProducts)
+    setBestSalesProducts(filterBestSalesProducts)
+  },[])
   return <Helmet title ={'Home'}>
       <section className='hero__section'>
         <Container>
@@ -34,6 +49,42 @@ const Home = () => {
         </Container>
     </section>
     <Services/>
+    <section className='recommend__products' >
+    <Container>
+          <Row>
+            <Col lg='12' className='text-center'>
+              <h2 className='section__title'>Recommended</h2>
+              <p className='section__recommend'>Complete your routine with </p>
+            </Col>
+            <ProductList data={recommendProducts}/>
+          </Row>
+        </Container>
+    </section>
+
+    <section className='best__sales'>
+      <Container>
+      <Row>
+            <Col lg='12' className='text-center'>
+            <h2 className='section__title'>Best Sales</h2>
+            </Col>
+
+            <ProductList data={bestSalesProducts}/>
+          </Row>
+      </Container>
+    </section>
+
+    <section className='timer_count'>
+    <Container>
+      <Row>
+            <Col lg='6' md='6'></Col>
+
+            <Col lg='6' md='6'>
+            <img src={counterImg} alt=''/>
+            </Col>
+
+          </Row>
+      </Container>
+    </section>
   </Helmet>
 
 }
