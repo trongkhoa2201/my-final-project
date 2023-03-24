6e97c3f62e2e217a2bbbc37e22c7cfbb61f77a26
trongkhoa2201 <84830390+trongkhoa2201@users.noreply.github.com>
2023-03-24 15:26:14 +0700

    Fix AdminNav

--- my-app/src/admin/AdminNav.js
+++ my-app/src/admin/AdminNav.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import React,{useRef} from 'react'
 import { Container, Row } from 'reactstrap'
+import {motion} from 'framer-motion'
 
 import useAuth from '../custom-hooks/useAuth'
 import '../styles/admin.css'
-import {NavLink} from 'react-router-dom'
+import {Link,NavLink,useNavigate} from 'react-router-dom'
+import { signOut } from 'firebase/auth'
+import { auth } from '../firebase.config'
+import { toast } from 'react-toastify'
+import userIcon from '../assets/images/user-icon.png'
 
 const admin_nav = [
     {
@@ -31,10 +36,28 @@ const admin_nav = [
 const AdminNav = () => {
 
     const {currentUser} = useAuth()
+    const navigate = useNavigate()
+    const profileActionRef = useRef(null)
+    const headerRef = useRef(null)
+
+    const menuRef = useRef(null)
     
+    const logout = () => {
+        signOut(auth).then(()=>{
+        toast.success('Logged out')
+        navigate('/home')
+        }).catch(err=>{
+        toast.error(err.message)
+        })
+    }
+
+    const menuToggle = () => menuRef.current.classList.toggle('active__menu')
+
+    const toggleProfileActions = () => profileActionRef.current.classList.toggle('show__profileActions')
+
     return (
         <>
-            <header className='admin-header'>
+            <header className='admin-header' ref={headerRef}>
                     <div className='admin-nav-top'>
                         <Container>
                             <div className='admin-nav-wrapper-top'>
@@ -49,9 +72,45 @@ const AdminNav = () => {
                                 <div className='admin-nav-top-right'>
                                     <span><i class="ri-notification-3-line"></i></span>
                                     <span><i class="ri-settings-4-line"></i></span>
-                                    <img src={currentUser && currentUser.photoURL} alt=''/>
+                                    <div className='profile'>
+                                    <motion.img 
+                                        whileTap={{ scale: 1.2 }} 
+                                        src={ currentUser ? currentUser.photoURL : userIcon} 
+                                        alt=''
+                                        onClick={toggleProfileActions}/>
+
+                                    <div className='profile__actions' 
+                                            ref={profileActionRef} 
+                                            onClick={toggleProfileActions}>
+                                        {
+                                        currentUser ? ( 
+                                            <div>
+                                            <Link to='/profile'>Profile</Link>
+                                            <br></br>
+                                            <span onClick={logout}>Logout</span>
+                                            </div>) 
+                                        : (
+                                        <div className=' d-flex align-items-center justify-content-center flex-column'>
+                                            <Link to='/signup'>Signup</Link>
+                                            <Link to='/login'>Login</Link>
+                                        </div>
+                                        )
+                                        }
+                                    </div>
+                                    </div>
                                 </div>
+
+                                {/* <div className='navigation' ref={menuRef} onClick={menuToggle}>
+                                <ul className='menu'>
+                                {
+                                    admin_nav.map((item , index) =>(<li className='nav__item' key={index}>
+                                    <NavLink to={item.path} className={(navClass)=> navClass.isActive ? 'nav__active' : ''}>{item.display}</NavLink>
+                                </li>))
+                                }
+                                </ul>
+                                </div> */}
                             </div>
+
                         </Container>
                     </div>
             </header>
